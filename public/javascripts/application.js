e38346c9dd1cb89f04eea77635f56f2b65efed1e
Joe Lind <joelind@gmail.com>
2010-01-26 09:09:54 -0500

    Allow edit/new form to degrade gracefully

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
     $('input#url_href, input#url_tag_list').focus(function(){
       this.value = $.trim(this.value);
+      $(this).addClass('highlight');
+
+      if($(this).parent('fieldset').hasClass('labels-inside'))
+        $('label[for=' + this.id + ']').hide();
+
       if(this.value == '' && this.id=='url_href')
         this.value = 'http://'
-      $(this).addClass('highlight');
-      $('label[for=' + this.id + ']').hide();
     });
     $('input#url_href, input#url_tag_list').blur(function(){
       $(this).removeClass('highlight');
@@ -12,8 +15,9 @@ $(document).ready(function() {
       if(this.value == 'http://' || this.value=='https://')
         this.value = '';
 
-      if(this.value=='')
+      if(this.value=='' && $(this).parent('fieldset').hasClass('labels-inside'))
         $('label[for=' + this.id + ']').show();
     });
+    $('fieldset#href-input, fieldset#taglist-input').addClass('labels-inside');
     $('input#url_href, input#url_tag_list').focus().blur();
 });
