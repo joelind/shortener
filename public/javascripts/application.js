be5d267682b72499e7117dde84ddc1aa7fcafd93
Joe Lind <joelind@gmail.com>
2010-01-26 02:58:41 -0500

    a little tiny bit of style for the edit page

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
     $('input#url_href, input#url_tag_list').focus(function(){
+      $(this).addClass('highlight');
       $('label[for=' + this.id + ']').hide();
     });
     $('input#url_href, input#url_tag_list').blur(function(){
+      $(this).removeClass('highlight');
       this.value = $.trim(this.value);
       if(this.value=='')
         $('label[for=' + this.id + ']').show();
