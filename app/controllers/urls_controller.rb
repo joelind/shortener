f762d06ce02f8ef00e7797f17e4ab94a2d39b083
Joe Lind <joelind@gmail.com>
2010-01-25 23:55:33 -0500

    make urls controller use the to_param / find_by_encoded_id to find urls

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -4,20 +4,30 @@ class UrlsController < ApplicationController
   end
 
   def new
-    @url_obj = Url.new
+    @url_obj = Url.new(:href => 'http://')
   end
 
   def create
     @url_obj = Url.find_by_href(params['url']['href']) || Url.new(params['url'])
     if @url_obj.save
-      return redirect_to :action => :show, :id => @url_obj.id
+      return redirect_to :action => :show, :id => @url_obj.to_param
     else
       render :action => :new
     end
   end
 
   def show
-    @url_obj = Url.find_by_id(params[:id])
+    @url_obj = Url.find_by_encoded_id(params[:id])
     return error_404 if @url_obj.nil?
   end
+
+  def redirect
+    @url_obj = Url.find_by_encoded_id(params[:id])
+    if @url_obj.present?
+      redirect_to @url_obj.href, :status => :moved_permanently
+    else
+      error_404
+    end
+  end
+
 end
--- test/functional/urls_controller_test.rb
+++ test/functional/urls_controller_test.rb
@@ -23,7 +23,7 @@ class UrlsControllerTest < ActionController::TestCase
 
     post :create, 'url' => params
 
-    assert_redirected_to :action => 'show', :id => 10
+    assert_redirected_to :action => 'show', :id => stubbed_url.to_param
   end
 
   test "post to create when the url is not duplicated and save fails" do
@@ -40,12 +40,32 @@ class UrlsControllerTest < ActionController::TestCase
     assert_equal stubbed_url, assigns(:url_obj)
   end
 
-  test "get to show for a non-existant url should return a 400" do
-    Url.expects(:find_by_id).with('999').returns(nil)
+  test "get to show for a non-existant url should return a 404" do
+    Url.expects(:find_by_encoded_id).with('999').returns(nil)
 
     get :show, 'id' => '999'
 
     assert_response :not_found
     assert_nil assigns(:url_obj)
+  end
+
+  test "get to redirect for an existant url should redirect to the appropriate href" do
+    url = stub(:href => 'http://scvngr.com')
+    Url.expects(:find_by_encoded_id).with('Ab123').returns(url)
+
+    get :redirect, :id => 'Ab123'
+
+    #moved permanently (301) is the right thing for this redirector since the current
+    # implementation's urls are immutable
+    assert_response :moved_permanently
+    assert_redirected_to url.href
+  end
+
+  test "get to redirect for a non-existant url should return a 404" do
+    Url.expects(:find_by_encoded_id).with('Ab123').returns(nil)
+
+    get :redirect, :id => 'Ab123'
+
+    assert_response :not_found
   end
 end
