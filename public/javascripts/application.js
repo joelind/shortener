39f805afab13d9aae7202cdc57f29af71fb0fbc6
Joe Lind <joelind@gmail.com>
2010-01-26 03:02:57 -0500

    let's make users not have to type in http:// every time

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,17 @@
 $(document).ready(function() {
     $('input#url_href, input#url_tag_list').focus(function(){
+      this.value = $.trim(this.value);
+      if(this.value == '' && this.id=='url_href')
+        this.value = 'http://'
       $(this).addClass('highlight');
       $('label[for=' + this.id + ']').hide();
     });
     $('input#url_href, input#url_tag_list').blur(function(){
       $(this).removeClass('highlight');
       this.value = $.trim(this.value);
+      if(this.value == 'http://' || this.value=='https://')
+        this.value = '';
+
       if(this.value=='')
         $('label[for=' + this.id + ']').show();
     });
