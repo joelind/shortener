5e9aa7866b73cf1729d0f924ba94daf3b41350da
Joe Lind <joelind@gmail.com>
2010-01-26 01:03:58 -0500

    Add foreigner

--- config/environment.rb
+++ config/environment.rb
@@ -21,6 +21,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'haml', :version => '2.2.17'
   config.gem 'will_paginate', :version => '2.3.11'
+  config.gem "matthuhiggins-foreigner", :lib => "foreigner", :version => '0.3.1'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
