50d740c8aa72a864cfd2ead95b81f73e9e18e16d
Joe Lind <joelind@gmail.com>
2010-01-25 22:29:53 -0500

    Do some validation on the URIs that we get (also ensure http/https)

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,4 +1,15 @@
 class Url < ActiveRecord::Base
   validates_uniqueness_of :href, :case_sensitive => false
   validates_presence_of :href
+  validate :href_is_valid
+
+  private
+  def href_is_valid
+    begin
+      uri = URI.parse(self.href)
+      self.errors.add(:href, "is invalid") unless ['http', 'https'].include?(uri.scheme)
+    rescue URI::InvalidURIError => e
+      self.errors.add(:href, "is invalid")
+    end
+  end
 end
--- test/unit/url_test.rb
+++ test/unit/url_test.rb
@@ -4,4 +4,7 @@ class UrlTest < ActiveSupport::TestCase
   should_have_db_index :href, :unique => true
   should_validate_uniqueness_of :href, :case_sensitive => :false
   should_validate_presence_of :href
+
+  should_allow_values_for :href, "http://www.foo.bar", "https://foo.bar"
+  should_not_allow_values_for :href, "xxx", "123", "/aaa", "mailto:joelind@gmail.com"
 end
